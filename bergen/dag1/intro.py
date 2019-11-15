2391101e48d58f53ef1baa0b609ec9e18974c113
MarieRoald <roald.marie@gmail.com>
2019-11-15 20:06:14 +0100

    Create intro.py

---
+++ bergen/dag1/intro.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 13 15:02:15 2019
+
+@author: Marie
+"""
+
+#https://github.com/kodeskolen/tekna_h19
+
+print('Python er kult!')
+
+
+navn = 'Marie'
+
+alder = 26
+
+print('Heisann', navn)
